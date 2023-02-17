21bb2f00782399c89b3d64f971a8db785151eea1
AttackBlueSteel <yfang53@asu.edu>
2023-02-16 18:44:57 -0700

    Second commit

--- project-part-1/src/main/java/MyCode.java
+++ project-part-1/src/main/java/MyCode.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 public class MyCode {
     static Graph<String, DefaultEdge> graph;
     @Override
+    //print the information of the graph
     public String toString() {
         return ("The number of nodes: " + graph.vertexSet().size() + "\n" +
                 "The label of the nodes: " + graph.vertexSet() + "\n" +
@@ -19,11 +20,22 @@ public class MyCode {
                 "The Nodes and the edge directions of the edges: " + (graph.edgeSet() + "").replace(":", "->"));
     }
 
+    //for testing
     public static void main(String[] args) throws IOException {
+        //feature 1
         MyCode myGraph = new MyCode();
         parseGraph("src/testFiles/dotGraph.dot");
         System.out.println(myGraph);
         outputGraph("src/testFiles/output.txt");
+        //feature 2
+        addNode("test_node");
+        System.out.println("after adding a node:\n" + myGraph);
+        removeNode("test_node");
+        System.out.println("after removing a node:\n" + myGraph);
+        addNodes(new String[]{"test_node1", "test_node2", "test_node3"});
+        System.out.println("after adding three nodes:\n" + myGraph);
+        removeNodes(new String[]{"test_node1", "test_node2", "test_node3"});
+        System.out.println("after removing three nodes:\n" + myGraph);
     }
 
     public static void parseGraph(String filepath) throws IOException {
@@ -40,4 +52,31 @@ public class MyCode {
         file_writer.write(dot_format);
         file_writer.close();
     }
+
+    public static void addNode(String label) {
+        if(!graph.vertexSet().contains(label)) {
+            graph.addVertex(label);
+        }
+    }
+
+    public static void addNodes(String[] label) {
+        for(int i = 0; i < label.length; i++){
+            addNode(label[i]);
+        }
+    }
+
+    public static void removeNode(String label) {
+        if(graph.vertexSet().contains(label)) {
+            graph.removeVertex(label);
+        }
+    }
+
+    public static void removeNodes(String[] label) {
+        for(int i = 0; i < label.length; i++) {
+            removeNode(label[i]);
+        }
+    }
+
+
+
 }
