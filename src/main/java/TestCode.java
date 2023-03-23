30c1e528a181938038d3b2a7311707388c93771c
AttackBlueSteel <yfang53@asu.edu>
2023-03-22 23:54:06 -0700

    Part2 11th Main

--- src/main/java/TestCode.java
+++ src/main/java/TestCode.java
@@ -7,20 +7,58 @@ import java.nio.file.Paths;
 
 
 public class TestCode {
-    static MyCode demo;
+    MyCode myGraph;
+
     @Before
     public void setup() throws Exception {
-        demo = new MyCode();
-        demo.parseGraph("src/testFiles/dotGraph.dot");
+        myGraph = new MyCode();
+        myGraph.parseGraph("src/testFiles/dotGraph.dot");
     }
     @Test
-    public void testDFSSearch() throws IOException {
-        Path result = demo.GraphSearch("v1","v6");
-        Assert.assertEquals(result.toString(),"v1->v2->v5->v6");
+    public void testFeature1() throws IOException {
+        Assert.assertEquals(13,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(26,myGraph.graph.edgeSet().size());
+        Assert.assertTrue(myGraph.graph.containsEdge("0", "2"));
+        myGraph.outputGraph("src/testFiles/output.txt");
     }
+
     @Test
-    public void testBFSSearch() throws IOException {
-        Path result = demo.GraphSearch("v1","v6");
-        Assert.assertEquals(result.toString(),"v1->v3->v6");
+    public void testFeature2() throws IOException{
+        myGraph.addNode("test_node");
+        Assert.assertEquals(14,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(26,myGraph.graph.edgeSet().size());
+        myGraph.removeNode("test_node");
+        Assert.assertEquals(13,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(26,myGraph.graph.edgeSet().size());
+        myGraph.addNodes(new String[]{"test_node1", "test_node2", "test_node3"});
+        Assert.assertEquals(16,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(26,myGraph.graph.edgeSet().size());
+        myGraph.removeNodes(new String[]{"test_node1", "test_node2", "test_node3"});
+        Assert.assertEquals(13,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(26,myGraph.graph.edgeSet().size());
+    }
+
+    @Test
+    public void testFeature3() throws IOException{
+        myGraph.addNodes(new String[]{"test_node1", "test_node2"});
+        myGraph.addEdge("test_node1","test_node2");
+        Assert.assertEquals(15,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(27,myGraph.graph.edgeSet().size());
+        Assert.assertTrue(myGraph.graph.containsEdge("test_node1", "test_node2"));
+        myGraph.removeEdge("test_node1","test_node2");
+        myGraph.removeNodes(new String[]{"test_node1", "test_node2"});
+        Assert.assertEquals(13,myGraph.graph.vertexSet().size());
+        Assert.assertEquals(26,myGraph.graph.edgeSet().size());
+        Assert.assertFalse(myGraph.graph.containsEdge("test_node1", "test_node2"));
+    }
+
+    @Test
+    public void testFeature4() throws IOException{
+        myGraph.outputDOTGraph("src/testFiles/output.dot");
+        myGraph.outputGraphics("src/testFiles/output.jpg", "JPG");
+        myGraph.outputGraphics("src/testFiles/output.png", "PNG");
+        String output = Files.readString(Paths.get("src/testFiles/output.dot"));
+        String expected = Files.readString(Paths.get("src/testFiles/expected.txt"));
+        Assert.assertEquals(expected, output);
     }
 }
